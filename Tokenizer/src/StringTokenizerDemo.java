d0fec8d77be245117bfef262dd00068e452fc8f6
ARBIND <ARBIND@HONEY>
2015-08-11 15:27:52 +0530

    Initial checkin complete

---
+++ Tokenizer/src/StringTokenizerDemo.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+
+public class StringTokenizerDemo {
+	
+	public static void main(String[] args){
+		
+		String str = "name=arbind;title=sahu";
+		Map<String, String> tokenMap = new HashMap<String, String>();
+		String[] splits = null;
+		
+		StringTokenizer token = new StringTokenizer(str,";");
+		while(token.hasMoreTokens()){
+			String element = token.nextToken();
+			splits = element.split("=");
+			tokenMap.put(splits[0], splits[1]);
+			System.out.println(tokenMap.toString());
+		}
+		
+	}
+}
